650ca6de1aa15825a245e673ef31b490dcf4dd5d
Quan <qmenq07@gmail.com>
2021-12-17 09:43:05 +0700

    feat: add role playing game

--- role-playing-game/src/lib.rs
+++ role-playing-game/src/lib.rs
@@ -1,7 +1,3 @@
-// This stub file contains items which aren't used yet; feel free to remove this module attribute
-// to enable stricter warnings.
-#![allow(unused)]
-
 pub struct Player {
     pub health: u32,
     pub mana: Option<u32>,
@@ -10,10 +6,39 @@ pub struct Player {
 
 impl Player {
     pub fn revive(&self) -> Option<Player> {
-        unimplemented!("Revive this player")
+        match self.health {
+            0 => {
+                let mana = match self.level {
+                    1..=9 => None,
+                    10.. => Some(100),
+                    _ => self.mana
+                };
+
+                return Some(Self { health: 100, mana: mana, level: self.level });
+            },
+            _ => None,
+        }
     }
 
     pub fn cast_spell(&mut self, mana_cost: u32) -> u32 {
-        unimplemented!("Cast a spell of cost {}", mana_cost)
+        match self.mana {
+            Some(mana) => {
+                if mana >= mana_cost {
+                    self.mana = Some(mana - mana_cost);
+                    return mana_cost * 2;
+                }
+
+                return 0;
+            },
+            None => {
+                if self.health >= mana_cost {
+                    self.health -= mana_cost;
+                } else {
+                    self.health = 0;
+                }
+
+                return 0;
+            }
+        }
     }
 }
